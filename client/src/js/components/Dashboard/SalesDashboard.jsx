8969476d627d70f83e935a5d2908e29bd5e5708b
Rahul Ravindran <rahulr1116@gmail.com>
2020-04-18 23:17:37 +0530

    [Client] Started the EventsDashboard design

--- client/src/js/components/Dashboard/SalesDashboard.jsx
+++ client/src/js/components/Dashboard/SalesDashboard.jsx
@@ -7,7 +7,38 @@ const SalesDashboard = () => {
             <DashboardNav />
             <section id='mainDashboard'>
                 <h2>overview</h2>
-                <div className='overview'></div>
+                {/* Global Analytics for all events  */}
+                <div className='overviewAnalyticsWrapper'>
+                    <div className='globalAnalytics'>
+                        <p className='subtleTitle'>total sales</p>
+                        <p className='mainTitle'>$2,233</p>
+                        <p className='percentageTag'>▲ 35.20%</p>
+                    </div>
+                    <div className='globalAnalytics'>
+                        <p className='subtleTitle'>registrations</p>
+                        <p className='mainTitle'>$2,233</p>
+                        <p className='percentageTag'>▼ 35.20%</p>
+                    </div>
+                    <div className='globalAnalytics'>
+                        <p className='subtleTitle'>total attendees</p>
+                        <p className='mainTitle'>$2,233</p>
+                        <p className='percentageTag'>▲ 35.20%</p>
+                    </div>
+                    <div className='globalAnalytics'>
+                        <p className='subtleTitle'>create new event</p>
+                        <p className='mainTitle'>+</p>
+                        {/* <p className='percentageTag'>▲ 35.20%</p> */}
+                    </div>
+                </div>
+
+                {/* All the createdEvents Of User/Organization */}
+                <div className='allCreatedEvents'>
+                    <h4>All created events</h4>
+                    <div className='createdEvent'>
+                        <p className='bold'>GraphQL Asia 2020</p>
+                        <a href='#'>test transparentize</a>
+                    </div>
+                </div>
             </section>
         </section>
     );
--- client/src/js/components/Events.jsx
+++ client/src/js/components/Events.jsx
@@ -1,181 +1,149 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Components
-import Nav from "./Nav";
+import Nav from './Nav';
 
 const Events = () => {
     return (
         <>
             <Nav />
-            <section className="events">
-                <div className="events_heroContent">
+            <section className='events'>
+                <div className='events_heroContent'>
                     <h1>
-                        <h6 style={{ display: "inline", color: "var(--mediumBlue)" }}>#</h6> Find
-                        your event
+                        <h1
+                            style={{
+                                color: 'var(--mediumBlue)',
+                                fontSize: '80px',
+                                display: 'inline',
+                            }}
+                        >
+                            #
+                        </h1>{' '}
+                        Find your event
                     </h1>
-                    <p className="d">Find all the events near you</p>
+                    <p className='d'>Find all the events near you</p>
                 </div>
             </section>
-            <section className="eventsList">
+            <section className='eventsList'>
                 <h2>Feeling adventurous ?</h2>
 
                 <form>
                     <input
-                        placeholder="Location"
-                        type="text"
-                        name="eventLocation"
-                        id="eventLocation"
+                        placeholder='Location'
+                        type='text'
+                        name='eventLocation'
+                        id='eventLocation'
                     />
-                    <label htmlFor="eventCategory">Category</label>
+                    <label htmlFor='eventCategory'>Category</label>
                     <input
-                        placeholder="Category"
-                        type="text"
-                        name="eventCategory"
-                        id="eventCategory"
+                        placeholder='Category'
+                        type='text'
+                        name='eventCategory'
+                        id='eventCategory'
                     />
-                    <label htmlFor="eventDate">Date</label>
-                    <input placeholder="Date" type="date" name="eventDate" id="eventDate" />
-                    <button type="submit">Search</button>
+                    <label htmlFor='eventDate'>Date</label>
+                    <input placeholder='Date' type='date' name='eventDate' id='eventDate' />
+                    <button type='submit'>Search</button>
                 </form>
 
-                <div className="eventsList_eventsContainer">
-                    <div className="eventsList_eventsContainer_event">
-                        <h5 className="eventsList_eventsContainer_event_date">27 apr - 01 may</h5>
+                <div className='eventsList_eventsContainer'>
+                    <div className='eventsList_eventsContainer_event'>
+                        <p className='eventsList_eventsContainer_event_date'>27 apr - 01 may</p>
                         <img
-                            className="eventsList_eventsContainer_event_image"
-                            src="https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            alt=""
+                            className='eventsList_eventsContainer_event_image'
+                            src='https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                            alt=''
                         />
-                        <div className="eventsList_eventsContainer_event_details">
-                            <h4>Event title</h4>
+                        <div className='eventsList_eventsContainer_event_details'>
+                            <h4>GraphQL Asia 2020. The megaevent in India</h4>
                             <span>
-                                Category: <strong>category</strong>
-                                <span className="country">country</span>
+                                Category: <strong>Coding Talk</strong>
+                                <span className='country'>India</span>
                             </span>
-                            <p>address</p>
+                            <p>Wakande Stadium and Auditorium, Churchgate</p>
                         </div>
-                        <div className="eventsList_eventsContainer_event_CTA">
+                        <div className='eventsList_eventsContainer_event_CTA'>
                             <Link to={`/events/dskjf`}>event details</Link>
                             <Link to={`/events/dskjf`}>remind me</Link>
                         </div>
                     </div>
-                    <div className="eventsList_eventsContainer_event">
-                        <h5 className="eventsList_eventsContainer_event_date">27 apr - 01 may</h5>
+                    <div className='eventsList_eventsContainer_event'>
+                        <p className='eventsList_eventsContainer_event_date'>27 apr - 01 may</p>
                         <img
-                            className="eventsList_eventsContainer_event_image"
-                            src="https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            alt=""
+                            className='eventsList_eventsContainer_event_image'
+                            src='https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                            alt=''
                         />
-                        <div className="eventsList_eventsContainer_event_details">
+                        <div className='eventsList_eventsContainer_event_details'>
                             <h4>Event title</h4>
                             <span>
                                 Category: <strong>category</strong>
-                                <span className="country">country</span>
+                                <span className='country'>country</span>
                             </span>
                             <p>address</p>
                         </div>
-                        <div className="eventsList_eventsContainer_event_CTA">
+                        <div className='eventsList_eventsContainer_event_CTA'>
                             <Link to={`/events/dskjf`}>event details</Link>
                             <Link to={`/events/dskjf`}>remind me</Link>
                         </div>
                     </div>
-                    <div className="eventsList_eventsContainer_event">
-                        <h5 className="eventsList_eventsContainer_event_date">27 apr - 01 may</h5>
+                    <div className='eventsList_eventsContainer_event'>
+                        <p className='eventsList_eventsContainer_event_date'>27 apr - 01 may</p>
                         <img
-                            className="eventsList_eventsContainer_event_image"
-                            src="https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            alt=""
+                            className='eventsList_eventsContainer_event_image'
+                            src='https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                            alt=''
                         />
-                        <div className="eventsList_eventsContainer_event_details">
+                        <div className='eventsList_eventsContainer_event_details'>
                             <h4>Event title</h4>
                             <span>
                                 Category: <strong>category</strong>
-                                <span className="country">country</span>
+                                <span className='country'>country</span>
                             </span>
                             <p>address</p>
                         </div>
-                        <div className="eventsList_eventsContainer_event_CTA">
+                        <div className='eventsList_eventsContainer_event_CTA'>
                             <Link to={`/events/dskjf`}>event details</Link>
                             <Link to={`/events/dskjf`}>remind me</Link>
                         </div>
                     </div>
-                    <div className="eventsList_eventsContainer_event">
-                        <h5 className="eventsList_eventsContainer_event_date">27 apr - 01 may</h5>
+                    <div className='eventsList_eventsContainer_event'>
+                        <p className='eventsList_eventsContainer_event_date'>27 apr - 01 may</p>
                         <img
-                            className="eventsList_eventsContainer_event_image"
-                            src="https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            alt=""
+                            className='eventsList_eventsContainer_event_image'
+                            src='https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                            alt=''
                         />
-                        <div className="eventsList_eventsContainer_event_details">
+                        <div className='eventsList_eventsContainer_event_details'>
                             <h4>Event title</h4>
                             <span>
                                 Category: <strong>category</strong>
-                                <span className="country">country</span>
+                                <span className='country'>country</span>
                             </span>
                             <p>address</p>
                         </div>
-                        <div className="eventsList_eventsContainer_event_CTA">
+                        <div className='eventsList_eventsContainer_event_CTA'>
                             <Link to={`/events/dskjf`}>event details</Link>
                             <Link to={`/events/dskjf`}>remind me</Link>
                         </div>
                     </div>
-                    <div className="eventsList_eventsContainer_event">
-                        <h5 className="eventsList_eventsContainer_event_date">27 apr - 01 may</h5>
+                    <div className='eventsList_eventsContainer_event'>
+                        <p className='eventsList_eventsContainer_event_date'>27 apr - 01 may</p>
                         <img
-                            className="eventsList_eventsContainer_event_image"
-                            src="https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            alt=""
+                            className='eventsList_eventsContainer_event_image'
+                            src='https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+                            alt=''
                         />
-                        <div className="eventsList_eventsContainer_event_details">
+                        <div className='eventsList_eventsContainer_event_details'>
                             <h4>Event title</h4>
                             <span>
                                 Category: <strong>category</strong>
-                                <span className="country">country</span>
+                                <span className='country'>country</span>
                             </span>
                             <p>address</p>
                         </div>
-                        <div className="eventsList_eventsContainer_event_CTA">
-                            <Link to={`/events/dskjf`}>event details</Link>
-                            <Link to={`/events/dskjf`}>remind me</Link>
-                        </div>
-                    </div>
-                    <div className="eventsList_eventsContainer_event">
-                        <h5 className="eventsList_eventsContainer_event_date">27 apr - 01 may</h5>
-                        <img
-                            className="eventsList_eventsContainer_event_image"
-                            src="https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            alt=""
-                        />
-                        <div className="eventsList_eventsContainer_event_details">
-                            <h4>Event title</h4>
-                            <span>
-                                Category: <strong>category</strong>
-                                <span className="country">country</span>
-                            </span>
-                            <p>address</p>
-                        </div>
-                        <div className="eventsList_eventsContainer_event_CTA">
-                            <Link to={`/events/dskjf`}>event details</Link>
-                            <Link to={`/events/dskjf`}>remind me</Link>
-                        </div>
-                    </div>
-                    <div className="eventsList_eventsContainer_event">
-                        <h5 className="eventsList_eventsContainer_event_date">27 apr - 01 may</h5>
-                        <img
-                            className="eventsList_eventsContainer_event_image"
-                            src="https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            alt=""
-                        />
-                        <div className="eventsList_eventsContainer_event_details">
-                            <h4>Event title</h4>
-                            <span>
-                                Category: <strong>category</strong>
-                                <span className="country">country</span>
-                            </span>
-                            <p>address</p>
-                        </div>
-                        <div className="eventsList_eventsContainer_event_CTA">
+                        <div className='eventsList_eventsContainer_event_CTA'>
                             <Link to={`/events/dskjf`}>event details</Link>
                             <Link to={`/events/dskjf`}>remind me</Link>
                         </div>
@@ -183,9 +151,9 @@ const Events = () => {
                 </div>
 
                 <img
-                    style={{width: "20%"}}
-                    src="https://qrtag.net/api/qr_transparent_1.svg?url=https://2020.teamtanay.jobchallenge.dev"
-                    alt=""
+                    style={{ width: '20%' }}
+                    src='https://qrtag.net/api/qr_transparent_1.svg?url=https://2020.teamtanay.jobchallenge.dev'
+                    alt=''
                 />
             </section>
         </>
